44b56ccb0ae270e2363fcee9c061605e641f483d
Liam Boreback <boreback.work@gmail.com>
2023-12-06 16:19:35 +0100

    work on day 5

--- src/day5.rs
+++ src/day5.rs
@@ -2,177 +2,126 @@
 
 use std::{cmp::Ordering, ops::Range};
 
-struct RangeMap {
-    ranges: Vec<(Range<usize>, isize)>,
+/*
+*
+* NOT WORKING
+* Solution inspired from: https://github.com/womogenes/AoC-2023-Solutions/blob/main/day_05/day_05_p2.py#L13
+*
+*/
+
+fn range_contains_range<T: PartialOrd>(this: &Range<T>, other: &Range<T>) -> bool {
+    this.start <= other.start && this.end >= other.end
 }
 
-impl RangeMap {
-    pub fn new(mut key_values: Vec<(Range<usize>, isize)>) -> Self {
-        key_values.sort_by(|(range1, _), (range2, _)| RangeMap::cmp_range_range(range1, range2));
-        Self { ranges: key_values }
-    }
+fn remap(range: &Range<usize>, layer: &[(Range<usize>, isize)]) -> Vec<Range<usize>> {
+    let mut answer = Vec::new();
 
-    pub fn get_key(&self, key: usize) -> Option<isize> {
-        let found = self
-            .ranges
-            .binary_search_by(|vec| RangeMap::cmp_range_num(&vec.0, &key))
-            .ok();
-        if found.is_none() {
-            return None;
-        }
-        Some(self.ranges[found.unwrap()].1)
-    }
-
-    pub fn get_new_value_from_map(&self, key: usize) -> usize {
-        let x = key as isize + self.get_key(key).unwrap_or(0);
-        return x as usize;
-    }
-
-    fn cmp_range_num(range: &Range<usize>, compare: &usize) -> Ordering {
-        if range.contains(&compare) {
-            return Ordering::Equal;
-        } else if range.start < *compare {
-            return Ordering::Less;
-        } else {
-            return Ordering::Greater;
+    for (layer_range, offset) in layer {
+        // if layer_range contains range
+        if range_contains_range(&layer_range, &range) {
+            answer.push((range.clone(), *offset));
         }
     }
 
-    fn cmp_range_range(range1: &Range<usize>, range2: &Range<usize>) -> Ordering {
-        if range1.end <= range2.start {
-            return Ordering::Less;
-        } else {
-            return Ordering::Greater;
+    // check if there are any spacing
+    {
+        let mut temp: Vec<(Range<usize>, isize)> = Vec::new();
+        for window in answer.windows(2) {
+            let (ans_range, _) = &window[0];
+            let (ans_range2, _) = &window[1];
+
+            if ans_range.end <= ans_range.start {
+                temp.push((ans_range.end..ans_range2.start, 0));
+            }
         }
+        answer.append(&mut temp);
     }
+
+    if answer.len() == 0 {
+        return vec![range.clone()];
+    }
+
+    let first_start = answer.first().unwrap().0.start;
+    if first_start > range.start {
+        answer.push((range.start..first_start, 0))
+    }
+    let last_finish = answer.last().unwrap().0.end;
+    if last_finish < range.end {
+        answer.push((last_finish..range.end, 0));
+    }
+
+    answer
+        .iter()
+        .map(|(range, offset)| {
+            (range.start as isize + offset) as usize..(range.end as isize + offset) as usize
+        })
+        .collect()
 }
 
-struct Mapping {
-    seed_to_soil: RangeMap,
-    soil_to_fert: RangeMap,
-    fert_to_water: RangeMap,
-    water_to_light: RangeMap,
-    light_to_temp: RangeMap,
-    temp_to_humid: RangeMap,
-    humid_to_loc: RangeMap,
+/// Accepts array of lines.
+fn parse_section(lines: &str) -> Vec<(Range<usize>, isize)> {
+    lines
+        .lines().skip(1)
+        .map(|line| {
+            let nums = line
+                .split_whitespace()
+                .map(|x| x.parse::<usize>().unwrap())
+                .collect::<Vec<_>>();
+            (
+                nums[1]..nums[1] + nums[2],
+                nums[0] as isize - nums[1] as isize,
+            )
+        })
+        .collect::<Vec<_>>()
 }
 
-impl Mapping {
-    pub fn parse_input(s: &[&[&str]]) -> Self {
-        let mut section_iter = s.into_iter();
-        // dest src length
+fn get_lowest_loc(seeds: Vec<Range<usize>>, maps: Vec<Vec<(Range<usize>, isize)>>) -> usize {
+    let mut lowest = usize::MAX;
 
-        let seed_to_soil;
-        {
-            let key_values = Mapping::parse_section(&section_iter.next().unwrap()[1..]);
-            seed_to_soil = RangeMap::new(key_values)
+    for seed in seeds.into_iter() {
+        let mut cur_ranges = vec![seed];
+        let mut new_ranges = Vec::new();
+
+        for map in maps.iter() {
+            for range in cur_ranges.iter() {
+                new_ranges.append(&mut remap(range, map));
+            }
         }
 
-        let soil_to_fert;
-        {
-            let key_values = Mapping::parse_section(&section_iter.next().unwrap()[1..]);
-            soil_to_fert = RangeMap::new(key_values)
-        }
+        cur_ranges = new_ranges;
+        new_ranges = vec![];
 
-        let fert_to_water;
-        {
-            let key_values = Mapping::parse_section(&section_iter.next().unwrap()[1..]);
-            fert_to_water = RangeMap::new(key_values)
-        }
-
-        let water_to_light;
-        {
-            let key_values = Mapping::parse_section(&section_iter.next().unwrap()[1..]);
-            water_to_light = RangeMap::new(key_values)
-        }
-
-        let light_to_temp;
-        {
-            let key_values = Mapping::parse_section(&section_iter.next().unwrap()[1..]);
-            light_to_temp = RangeMap::new(key_values)
-        }
-
-        let temp_to_humid;
-        {
-            let key_values = Mapping::parse_section(&section_iter.next().unwrap()[1..]);
-            temp_to_humid = RangeMap::new(key_values)
-        }
-
-        let humid_to_loc;
-        {
-            let key_values = Mapping::parse_section(&section_iter.next().unwrap()[1..]);
-            humid_to_loc = RangeMap::new(key_values)
-        }
-
-        Self {
-            seed_to_soil,
-            soil_to_fert,
-            fert_to_water,
-            water_to_light,
-            light_to_temp,
-            temp_to_humid,
-            humid_to_loc,
-        }
+        lowest = lowest.min(cur_ranges.iter().map(|x| x.start).min().unwrap())
     }
 
-    /// Accepts array of lines.
-    fn parse_section(lines: &[&str]) -> Vec<(Range<usize>, isize)> {
-        lines
-            .iter()
-            .map(|line| {
-                let nums = line
-                    .split_whitespace()
-                    .map(|x| x.parse::<usize>().unwrap())
-                    .collect::<Vec<_>>();
-                (
-                    nums[1]..nums[1] + nums[2],
-                    nums[0] as isize - nums[1] as isize,
-                )
-            })
-            .collect::<Vec<_>>()
-    }
-
-    pub fn get_loc(&self, seed: usize) -> usize {
-        let soil = self.seed_to_soil.get_new_value_from_map(seed);
-        let fert = self.soil_to_fert.get_new_value_from_map(soil);
-
-        let water = self.fert_to_water.get_new_value_from_map(fert);
-        let light = self.water_to_light.get_new_value_from_map(water);
-        let temp = self.light_to_temp.get_new_value_from_map(light);
-        let humid = self.temp_to_humid.get_new_value_from_map(temp);
-        let loc = self.humid_to_loc.get_new_value_from_map(humid);
-
-        return loc;
-    }
+    lowest
 }
 
 mod part1 {
-    use super::Mapping;
+    use super::{get_lowest_loc, parse_section};
 
-    fn get_lowest_loc(s: &str) -> usize {
-        let paragraphs: Vec<_> = s
-            .split_terminator("\n\n") // Split into paragraphs
-            .map(|paragraph| paragraph.lines().collect::<Vec<_>>().into_boxed_slice()) // Collect into slice
-            .collect();
+    fn part1_loc(s: &str) -> usize {
+        let paragraphs: Vec<_> = s.split_terminator("\n\n").collect();
 
         // seeds
-        let seeds = paragraphs[0][0].split(": ").collect::<Vec<_>>()[1]
+        let seeds = paragraphs[0].split(": ").collect::<Vec<_>>()[1]
             .split_whitespace()
-            .map(|x| x.parse::<usize>().unwrap())
+            .map(|x| {
+                let x = x.parse::<usize>().unwrap();
+                x..x + 1
+            })
             .collect::<Vec<_>>();
 
-        let map = Mapping::parse_input(
-            &paragraphs[1..]
-                .iter()
-                .map(|boxed_slice| boxed_slice.as_ref())
-                .collect::<Vec<_>>(),
-        );
-        let locs = seeds.iter().map(|x| map.get_loc(*x)).collect::<Vec<_>>();
-        locs.into_iter().min().unwrap()
+        let maps = paragraphs[1..]
+            .iter()
+            .map(|para| parse_section(para))
+            .collect();
+
+        get_lowest_loc(seeds, maps)
     }
 
     mod tests {
-        use crate::day5::part1::get_lowest_loc;
+        use crate::day5::part1::{get_lowest_loc, part1_loc};
 
         #[test]
         fn test_whole_str() {
@@ -209,43 +158,19 @@ temperature-to-humidity map:
 humidity-to-location map:
 60 56 37
 56 93 4";
-            assert_eq!(get_lowest_loc(test_str), 35);
+            assert_eq!(part1_loc(test_str), 35);
         }
 
         #[test]
         fn test_final() {
             let test_file = std::fs::read_to_string("day5.txt").unwrap();
-            dbg!(get_lowest_loc(&test_file));
+            dbg!(part1_loc(&test_file));
         }
     }
 }
 
 mod part2 {
     use super::*;
-
-    fn get_lowest_loc(s: &str) -> usize {
-        let paragraphs: Vec<_> = s
-            .split_terminator("\n\n") // Split into paragraphs
-            .map(|paragraph| paragraph.lines().collect::<Vec<_>>().into_boxed_slice()) // Collect into slice
-            .collect();
-
-        // seeds
-        let seeds = paragraphs[0][0].split(": ").collect::<Vec<_>>()[1]
-            .split_whitespace()
-            .map(|x| x.parse::<usize>().unwrap())
-            .collect::<Vec<_>>();
-
-        let seeds: Vec<usize> = seeds.chunks(2).map(|window| window[0]..window[0]+window[1]).flat_map(|range| range.collect::<Vec<_>>()).collect();
-
-        let map = Mapping::parse_input(
-            &paragraphs[1..]
-                .iter()
-                .map(|boxed_slice| boxed_slice.as_ref())
-                .collect::<Vec<_>>(),
-        );
-        let locs = seeds.iter().map(|x| map.get_loc(*x)).collect::<Vec<_>>();
-        locs.into_iter().min().unwrap()
-    }
 
     mod tests {
         use super::get_lowest_loc;
@@ -285,13 +210,13 @@ temperature-to-humidity map:
 humidity-to-location map:
 60 56 37
 56 93 4";
-            assert_eq!(get_lowest_loc(test_str), 46);
+            // assert_eq!(get_lowest_loc(test_str), 46);
         }
 
         #[test]
         fn test_final() {
             let test_file = std::fs::read_to_string("day5.txt").unwrap();
-            dbg!(get_lowest_loc(&test_file));
+            // dbg!(get_lowest_loc(&test_file));
         }
     }
 }
